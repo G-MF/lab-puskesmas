8d790b3149e1c2acde3ea0c34a1c15f773374d1e
G-MF <gusti.fahrubi@gmail.com>
2021-03-24 03:07:03 +0800

    tambah edit password

--- page/profil.php
+++ page/profil.php
@@ -47,7 +47,7 @@ $data = $koneksi->query("SELECT * FROM pasien p INNER JOIN user u ON p.id_user =
                                 <button type="button" data-toggle="modal" data-target="#edit-profil" class="btn btn-sm bg-gradient-purple mr-2">
                                     <i class="fa fa-edit"> Edit Profil</i>
                                 </button>
-                                <button type="button" data-toggle="modal" data-target="#edit-pass" class="btn btn-sm bg-gradient-dark">
+                                <button type="button" data-toggle="modal" data-target="#edit-pw" class="btn btn-sm bg-gradient-dark">
                                     <i class="fa fa-edit"> Edit Password</i>
                                 </button>
                             </ol>
@@ -201,7 +201,7 @@ $data = $koneksi->query("SELECT * FROM pasien p INNER JOIN user u ON p.id_user =
                         </div>
 
                         <div class="modal-footer justify-content-center">
-                            <button class="btn bg-gradient-primary text-white" type="submit" name="edit-pw">
+                            <button class="btn bg-gradient-primary text-white" type="submit" name="edit-profil">
                                 <i class="fa fa-save"> Simpan</i>
                             </button>
                             <button class="btn bg-gradient-dark text-white" type="button" data-dismiss="modal">
@@ -214,6 +214,55 @@ $data = $koneksi->query("SELECT * FROM pasien p INNER JOIN user u ON p.id_user =
             </div>
         </div>
         <!-- // MODAL EDIT PROFIL -->
+
+
+        <!-- MODAL EDIT PASSWORD -->
+        <div class="modal fade" id="edit-pw" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title font-weight-bold">Ubah Password</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="" method="POST">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="pass_lama">Password Lama</label>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" name="pass_lama" id="pass_lama" minlength="5" maxlength="10" required>
+                                    <div class="input-group-append" id="btn_lama">
+                                        <button type="button" class="btn bg-gradient-dark" onclick="lihatpass('pass_lama');" title="Tampilkan Password"><i class="fas fa-eye-slash"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="pass_lama">Password Baru</label>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" name="pass_baru" id="pass_baru" minlength="5" maxlength="10" required>
+                                    <div class="input-group-append" id="btn_baru">
+                                        <button type="button" class="btn bg-gradient-dark" onclick="lihatpass('pass_baru');" title="Tampilkan Password"><i class="fas fa-eye-slash"></i></button>
+                                    </div>
+                                </div>
+                                <small class="text-muted font-italic">*Password Minimal 5 Karakter | Maksimal 10 Karakter</small>
+                            </div>
+                        </div>
+                        <div class="modal-footer justify-content-center">
+                            <button class="btn bg-gradient-primary text-white" type="submit" name="edit-pw">
+                                <i class="fa fa-save"> Simpan</i>
+                            </button>
+                            <button class="btn bg-gradient-dark text-white" type="button" data-dismiss="modal">
+                                <i class="fa fa-times"> Batal</i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+        <!-- // MODAL EDIT PASSWORD -->
 
 
         <!-- Control Sidebar -->
@@ -240,7 +289,7 @@ $data = $koneksi->query("SELECT * FROM pasien p INNER JOIN user u ON p.id_user =
 </html>
 
 <?php
-if (isset($_POST['edit-pw'])) {
+if (isset($_POST['edit-profil'])) {
     $id_pasien    = $data['id_pasien'];
     $id_user      = $data['id_user'];
     $no_ktp       = strip_tags($_POST['no_ktp']);
@@ -338,5 +387,39 @@ if (isset($_POST['edit-pw'])) {
             echo '<meta http-equiv="refresh" content="2; url="' . base_url('page/profil') . '">';
         }
     }
+} else
+
+if (isset($_POST['edit-pw'])) {
+    $id_user   = $data['id_user'];
+    $pass_lama = md5(strip_tags($_POST['pass_lama']));
+    $pass_baru = md5(strip_tags($_POST['pass_baru']));
+
+    $cek_pass_lama = $koneksi->query("SELECT * FROM user WHERE id_user = '$id_user' AND password = '$pass_lama'")->fetch_array();
+
+    if ($cek_pass_lama) {
+        $ubah = $koneksi->query("UPDATE user SET password = '$pass_baru' WHERE id_user = '$id_user'");
+        if ($ubah) {
+            echo "
+                <script type='text/javascript'>
+                Toast.fire({
+                    type: 'success',
+                    title: 'Password Berhasil Diubah'
+                });
+                </script>";
+            echo '<meta http-equiv="refresh" content="2; url="' . base_url('page/profil') . '">';
+        }
+    } else {
+        echo "
+            <script type='text/javascript'>
+            Toast.fire({
+                type: 'error',
+                title: 'Password Lama Yang Anda Masukkan Salah!'
+            });
+
+            $('#edit-pw').modal('show');
+            </script>";
+    }
 }
+
+
 ?>
